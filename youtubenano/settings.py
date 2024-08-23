5e926e3878831fc0c9070c42ac29894cf18b3538
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-08-23 18:37:45 +0800

    Add custom user model

--- youtubenano/settings.py
+++ youtubenano/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
     "rest_framework",
+    # Local apps
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.CustomUser"
