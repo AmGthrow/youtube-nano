a52947cb55c539a72c4faecf5552a76a572b7e7d
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-08-25 16:52:48 +0800

    serve uploaded video files

--- youtubenano/urls.py
+++ youtubenano/urls.py
@@ -1,3 +1,5 @@
+import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
@@ -13,3 +15,6 @@ urlpatterns = [
     path("api/v1/", include(router.urls)),
     path("", frontend_views.index),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)]
