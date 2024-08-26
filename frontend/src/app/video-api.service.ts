2726c3b78e244ff501bb5084f43649546086d3f7
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-08-26 15:23:14 +0800

    add hardcoded API data in VideoApiService

--- frontend/src/app/video-api.service.ts
+++ frontend/src/app/video-api.service.ts
@@ -8,31 +8,41 @@ export class VideoApiService {
 
   getVideo(id: string): VideoDataDetailed {
     return {
-      id: "d34cd0d1-00c2-43ec-b330-89ab0eaeeb38",
-      title: 'My Awesome Video',
-      video_file: 'https://file-examples.com/storage/fe45dfa76e66c6232a111c9/2017/04/file_example_MP4_1920_18MG.mp4',
-      description: 'This is a description of my awesome video.',
-      uploader: 'Jimmy Johnson',
-      likes: 456
+      "id": "fd6a037f-7f1a-4e95-b96b-32996e5fad35",
+      "video_file": "http://127.0.0.1:8000/media/videos/sample_960x540.mp4",
+      "title": "Beach lady",
+      "description": "Sample video that shows a lady by a beach",
+      "uploader": "testuser1",
+      "likes": 500,
+      "uploaded_at": "2024-08-26T07:13:10.081651Z"
     };
   };
   getVideos(): VideoData[] {
     return [
       {
-        id: "503a22b9-89fe-461b-8d1b-e4c0bc1e8368",
-        title: 'Video Two',
-        uploader: 'Billy Bobblekins',
+        "id": "fd6a037f-7f1a-4e95-b96b-32996e5fad35",
+        "thumbnail_file": "http://127.0.0.1:8000/media/thumbnails/videos/sample_960x540.jpg",
+        "title": "Beach lady",
+        "uploader": "testuser1"
       },
       {
-        id: "69d6bb04-3ded-428f-90a4-51f52d4fa9a3",
-        title: 'Video Three',
-        uploader: 'Freya Fax',
+        "id": "a986ab81-d1ea-4fbf-b12b-11cd83c933ef",
+        "thumbnail_file": "http://127.0.0.1:8000/media/thumbnails/videos/SampleVideo_1280x720_2mb.jpg",
+        "title": "Bunny movie",
+        "uploader": "testuser1"
       },
       {
-        id: "da723955-73b2-48e3-a146-45463b2b1784",
-        title: 'Video Four',
-        uploader: 'Derrick Dinglebop',
+        "id": "a0fc8b34-f907-4a37-83aa-7042c17691c3",
+        "thumbnail_file": "http://127.0.0.1:8000/media/thumbnails/videos/file_example_MP4_640_3MG_Y4cX00g.jpg",
+        "title": "testuser1's first upload",
+        "uploader": "testuser1"
       },
-    ]
-  }
-}
+      {
+        "id": "c85554be-a98e-4d8b-9869-f06035c76e58",
+        "thumbnail_file": "http://127.0.0.1:8000/media/thumbnails/videos/file_example_MP4_640_3MG.jpg",
+        "title": "First file uploaded via DRF",
+        "uploader": "Jethro"
+      }
+    ];
+  };
+};
